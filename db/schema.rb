7f3f88fa7d8f9aeeb79933d7703582974bd8ca8d
Lutvi Rosyady <moorails@gmail.com>
2018-04-05 15:36:09 +0700

    Updating DB with using PG

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180405070353) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "first_name"
